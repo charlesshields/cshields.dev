dc0c9b2819ff18acd10b6786e8ed2071ffdf6d66
Charles <charles.github@ghostrabbit.me>
2025-02-12 06:30:40 +0000

    hello

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 http.createServer(function (req, res) {
 	res.write("On the way to being a full stack engineer!");
+	res.write("I love you Mona! I made this website from the ground up!")
 	res.end();
 }).listen(3000);
 
